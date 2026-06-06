f58e2adfde9123f89a266aa1a10e5ce7b3f54f0f
Victor <161361798+pskflp@users.noreply.github.com>
2026-06-06 18:59:02 -0300

    setup inicial do projeto

---
+++ src/main/java/ES2/MonitoramentoCardiaco/MonitoramentoCardiacoApplication.java
@@ -0,0 +1,13 @@
+package ES2.MonitoramentoCardiaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoCardiacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoCardiacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ES2/MonitoramentoCardiaco/MonitoramentoCardiacoApplicationTests.java
@@ -0,0 +1,13 @@
+package ES2.MonitoramentoCardiaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoCardiacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
