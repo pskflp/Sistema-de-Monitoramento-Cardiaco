aadfff14d1acdf25ea70e7ce2f9fa328f788838e
Raphael Esch <raphael.esch@hotmail.com>
2026-06-09 01:30:22 -0300

    feat: adiciona UsuarioController com endpoint POST /usuarios/cadastro

---
+++ src/main/java/ES2/MonitoramentoCardiaco/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package ES2.MonitoramentoCardiaco.controller;
+
+import ES2.MonitoramentoCardiaco.dto.UsuarioCreateDTO;
+import ES2.MonitoramentoCardiaco.dto.UsuarioResponseDTO;
+import ES2.MonitoramentoCardiaco.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<UsuarioResponseDTO> cadastrar(@RequestBody @Valid UsuarioCreateDTO dto) {
+        UsuarioResponseDTO response = usuarioService.criarConta(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
